5bece383a67b752faac49ec1d71f2c06bd2a05f0
artur <artur.oliveirasantiago@gmail.com>
2024-03-26 21:56:12 -0300

    Estória de Usuário 1: Configuração inicial - backend init setup

---
+++ backend/src/test/java/com/pitangchallenge/usercars/CarsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitangchallenge.usercars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

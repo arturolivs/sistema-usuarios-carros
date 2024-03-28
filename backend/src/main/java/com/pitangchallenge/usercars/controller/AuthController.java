3cb8676477332b093ffd1483279c8ca800bb0a39
artur <artur.oliveirasantiago@gmail.com>
2024-03-28 14:52:42 -0300

    Estória de Usuário 4: Autenticação e Obtenção de Token - move auth and token logic to service

--- backend/src/main/java/com/pitangchallenge/usercars/controller/AuthController.java
+++ backend/src/main/java/com/pitangchallenge/usercars/controller/AuthController.java
@@ -2,13 +2,10 @@ package com.pitangchallenge.usercars.controller;
 
 import com.pitangchallenge.usercars.dto.SignInDTO;
 import com.pitangchallenge.usercars.dto.SignInResponseDTO;
-import com.pitangchallenge.usercars.model.User;
-import com.pitangchallenge.usercars.service.TokenService;
+import com.pitangchallenge.usercars.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private TokenService tokenService;
+@Autowired
+private AuthService authService;
 
     @PostMapping("/api/signin")
     public ResponseEntity signIn(@RequestBody @Valid SignInDTO data) {
-        var loginPassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(loginPassword);
-
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+        String token = authService.authenticate(data);
         return ResponseEntity.ok(new SignInResponseDTO(token));
     }
 }
--- backend/src/main/java/com/pitangchallenge/usercars/service/AuthService.java
+++ backend/src/main/java/com/pitangchallenge/usercars/service/AuthService.java
@@ -1,7 +1,12 @@
 package com.pitangchallenge.usercars.service;
 
+import com.pitangchallenge.usercars.dto.SignInDTO;
+import com.pitangchallenge.usercars.model.User;
 import com.pitangchallenge.usercars.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,8 +17,22 @@ public class AuthService implements UserDetailsService {
 
     @Autowired
     UserRepository repository;
+
+    @Autowired
+    @Lazy
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByLogin(username).get();
+    }
+
+    public String authenticate(SignInDTO data) {
+        var loginPassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(loginPassword);
+        return tokenService.generateToken((User) auth.getPrincipal());
     }
 }

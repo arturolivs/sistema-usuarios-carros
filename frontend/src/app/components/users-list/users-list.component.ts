5ac2d8cdca0a4d817ac319c43b0764769afc816a
artur <artur.oliveirasantiago@gmail.com>
2024-03-31 19:37:39 -0300

    Estória de Usuário 3: Listagem de Usuários - get users from server on front

--- frontend/src/app/components/users-list/users-list.component.ts
+++ frontend/src/app/components/users-list/users-list.component.ts
@@ -10,19 +10,8 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { User } from '../../models/user.model';
 import { RemoveUserDialogComponent } from '../remove-user-dialog/remove-user-dialog.component';
+import { UserService } from '../../services/user.service';
 
-const MOCK_USERS: User[] = [
-  {id: 1, firstName: 'Hydrogen', lastName: 'Hydrogen last',  email: 'Hydrogen@email.com' , login: 'Hydrogen_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 2, firstName: 'Helium', lastName: 'Helium last',  email: 'Helium@email.com' , login: 'Helium_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 3, firstName: 'Lithium', lastName: 'Lithium last',  email: 'Lithium@email.com' , login: 'Lithium_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 4, firstName: 'Beryllium', lastName: 'Beryllium last',  email: 'Beryllium@email.com' , login: 'Beryllium_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 5, firstName: 'Boron', lastName: 'Boron last',  email: 'Boron@email.com', login: 'Boron_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 6, firstName: 'Carbon', lastName: 'Carbon last',  email: 'Carbon@email.com', login: 'Carbon_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 7, firstName: 'Nitrogen', lastName: 'Nitrogen last',  email: 'Nitrogen@email.com', login: 'Nitrogen_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 8, firstName: 'Oxygen', lastName: 'Oxygen last',  email: 'Oxygen@email.com', login: 'Oxygen_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 9, firstName: 'Fluorine', lastName: 'Fluorine last',  email: 'Fluorine@email.com', login: 'Fluorine_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234'},
-  {id: 10, firstName: 'Neon', lastName: 'Neon last',  email: 'Neon@email.com', login: 'Neon_login', birthday: new Date(), password: 'Hydrogen_password', phone: '1234' }
-];
 
 @Component({
   selector: 'app-users-list',
@@ -33,9 +22,19 @@ const MOCK_USERS: User[] = [
 })
 export class UsersListComponent {
   displayedColumns: string[] = ['firstName', 'lastName', 'email','login' ,'actions'];
-  dataSource = MOCK_USERS;
+  data: User[] = [];
 
-  constructor(public dialog: MatDialog, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    public dialog: MatDialog,
+    private router: Router) {}
+
+
+    ngOnInit(): void {
+      this.userService.getAllUsers().subscribe(users => {
+        this.data = users;
+      });
+   }
 
   editUser(userId: number): void {
     this.router.navigate(['/user-form', userId]);
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -7,30 +7,15 @@ import { User } from "../models/user.model";
   providedIn: 'root'
  })
  export class UserService {
-  private apiUrl = 'http://localhost:3000/api/users';
+  private apiUrl = 'http://localhost:8080/api/users';
 
   constructor(private http: HttpClient) {}
 
-  // getUserById(id: string): Observable<any> {
-  //    return this.http.get(`${this.apiUrl}/${id}`);
-  // }
+  getUserById(id: string): Observable<any> {
+     return this.http.get(`${this.apiUrl}/${id}`);
+  }
 
-  getUserById(id: string): Observable<User> {
-    // Exemplo de objeto mockado
-    const mockUser: User = {
-       id: Number(id),
-       firstName: 'Nome Mockado',
-       lastName: 'Sobrenome Mockado',
-       email: 'email@mockado.com',
-       login: 'login-mockado',
-       password: 'pass',
-       birthday: new Date(),
-       phone: '1234'
-
-       // Adicione outros campos conforme necessário
-    };
-
-    // Retorna um Observable que emite o objeto mockado
-    return of(mockUser);
-   }
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
  }

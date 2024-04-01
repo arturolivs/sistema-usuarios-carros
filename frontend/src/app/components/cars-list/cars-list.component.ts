c43a8fed579afcfd3746665d4d0343e9e9c5f92a
artur <artur.oliveirasantiago@gmail.com>
2024-04-01 04:34:50 -0300

    Estória de Usuário 9: Listagem de Carros do Usuário Logado - created list cars

---
+++ frontend/src/app/components/cars-list/cars-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterModule } from '@angular/router';
+
+
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+import { Car } from '../../models/car.model';
+import { CarService } from '../../services/car.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cars-list',
+  standalone: true,
+  imports: [MatTableModule,MatButtonModule, MatIconModule,RouterModule],
+  templateUrl: './cars-list.component.html',
+  styleUrl: './cars-list.component.scss'
+})
+export class CarsListComponent {
+  displayedColumns: string[] = ['year', 'licensePlate', 'model', 'color', 'actions'];
+  data: Car[] = [];
+
+  constructor(
+     private carService: CarService,
+     public dialog: MatDialog,
+     private router: Router
+  ) {}
+
+  ngOnInit(): void {
+     this.carService.getAllCars().subscribe(cars => {
+       this.data = cars;
+     });
+  }
+ }
+
---
+++ frontend/src/app/services/car.service.ts
@@ -0,0 +1,26 @@
+
+ import { HttpClient, HttpHeaders } from '@angular/common/http';
+ import { Observable, switchMap } from 'rxjs';
+import { Car } from '../models/car.model';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+ @Injectable({
+  providedIn: 'root'
+ })
+ export class CarService {
+  private apiUrl = 'http://localhost:8080/api/cars';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getAllCars(): Observable<Car[]> {
+    return this.authService.token.pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        return this.http.get<Car[]>(this.apiUrl, { headers });
+      })
+    );
+ }
+
+
+ }

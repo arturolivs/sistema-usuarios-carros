fce2925a807923aac56a635868d2bf983248533a
artur <artur.oliveirasantiago@gmail.com>
2024-04-02 02:41:37 -0300

    Estória de Usuário 1: Configuração do projeto - using `env` on `cors config`

--- backend/src/main/java/com/pitangchallenge/usercars/config/CorsConfig.java
+++ backend/src/main/java/com/pitangchallenge/usercars/config/CorsConfig.java
@@ -1,20 +1,26 @@
 package com.pitangchallenge.usercars.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                String allowedOrigin = env.getProperty("allowed.origin", "http://localhost:4200");
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
